ef26de54a135927fc05b726d9ab21c208aff2a65
Roxxas96 <gomez.a.corneille@gmail.com>
2022-02-01 19:14:28 +0100

    Routes

--- app/routes/shop/index.tsx
+++ app/routes/shop/index.tsx
@@ -1,7 +1,58 @@
-export default function Shop () {
-    return(
-        <div>
-            <h1>Shop</h1>
-        </div>
-    )
+import { Link, LoaderFunction, useCatch, useLoaderData } from "remix";
+
+import { Goodies } from "~/models/Goodies";
+
+import { requireUserId } from "~/services/authentication";
+import { getManyGoodies } from "~/services/goodies";
+
+type LoaderData = {
+  goodies?: Goodies[];
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  await requireUserId(request, "/challenges");
+
+  const goodies = await getManyGoodies(request);
+
+  return { goodies: goodies };
+};
+
+export default function Shop() {
+  const data = useLoaderData<LoaderData>();
+  return (
+    <div>
+      <h1>Shop</h1>
+      {data.goodies?.map((goodie) => {
+        return (
+          <Link to={`/shop/${goodie.id}`} key={goodie.id}>
+            <h2>{goodie.name}</h2>
+            <p>Price : {goodie.price}</p>
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  return (
+    <div>
+      <h1>
+        {caught.status} {caught.statusText}
+      </h1>
+      <p>{caught.data}</p>
+    </div>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.error(error);
+  return (
+    <div>
+      <h1>Something went wrong</h1>
+      <p>{error.message}</p>
+    </div>
+  );
 }
