9f53f84b223a54c2db8aceafefd99a6b6c6fe0e8
Roxxas96 <gomez.a.corneille@gmail.com>
2022-03-24 18:15:17 +0100

    honor: Flo top1

--- app/components/user/userList.tsx
+++ app/components/user/userList.tsx
@@ -19,6 +19,7 @@ export default function UserList({
   users: User[];
   API_URL?: string;
 }) {
+  let cheatCode = false;
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -32,6 +33,58 @@ export default function UserList({
           </TableRow>
         </TableHead>
         <TableBody>
+          {/* <TableRow
+              key={0}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                <Link
+                  to={`/users/7`}
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  <Avatar
+                    src={`${
+                      API_URL || "http://localhost:4000/"
+                    }user/avatar/${"e2abfc1c697dda9c0c60e5e8f026544a7788a1bff29e4b269647295c15931e3d2b624fb2a4b990fae96bab3fd3b3cd9a"}`}
+                    alt={"Flo"}
+                    sx={{ width: 150, height: 150 }}
+                    style={{ margin: "auto" }}
+                  />
+                </Link>
+              </TableCell>
+              <TableCell align="center">
+                <Link
+                  style={{ textDecoration: "none", color: "black" }}
+                  to={`/users/7`}
+                >
+                  <b>{"Flo"}</b>
+                </Link>
+              </TableCell>
+              <TableCell align="center">
+                <Link
+                  style={{ textDecoration: "none", color: "black" }}
+                  to={`/users/7`}
+                >
+                  <b>{"Florent"}</b>
+                </Link>
+              </TableCell>
+              <TableCell align="center">
+                <Link
+                  style={{ textDecoration: "none", color: "black" }}
+                  to={`/users/7`}
+                >
+                  <b>{"Hugovieux"}</b>
+                </Link>
+              </TableCell>
+              <TableCell align="center">
+                <Link
+                  style={{ textDecoration: "none", color: "black" }}
+                  to={`/users/7`}
+                >
+                  <b>{99999999}</b>
+                </Link>
+              </TableCell>
+            </TableRow> */}
           {users.map((user) => (
             <TableRow
               key={user.id}
